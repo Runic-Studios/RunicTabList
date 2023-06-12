47e3ffcfdeb4444507d5d5cf7db05fae0268050c
BoBoBalloon <61363161+BoBoBalloon@users.noreply.github.com>
2023-06-11 21:30:46 -0400

    git init

--- src/main/java/com/runicrealms/runictablist/tab/TabList.java
+++ src/main/java/com/runicrealms/runictablist/tab/TabList.java
@@ -1,2 +1,8 @@
-package com.runicrealms.runictablist.tab;public class TabList {
+package com.runicrealms.runictablist.tab;
+
+/**
+ * A class that represents a tab list sent to a player
+ */
+public class TabList {
+
 }
