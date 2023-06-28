8841a7a8090dabae23d0ee7edebb06cea58e7163
Excel619 <excel8392@gmail.com>
2023-06-27 22:55:04 -0400

    Removed old gradle files, added as submodule

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+val artifactName = "tablist"
+val rrGroup: String by rootProject.extra
+val rrVersion: String by rootProject.extra
+
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+group = rrGroup
+version = rrVersion
+
+dependencies {
+    compileOnly(commonLibs.spigot)
+    compileOnly(commonLibs.paper)
+    compileOnly(commonLibs.protocollib)
+    compileOnly(project(":Projects:Common"))
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = rrGroup
+            artifactId = artifactName
+            version = rrVersion
+            from(components["java"])
+        }
+    }
+}
