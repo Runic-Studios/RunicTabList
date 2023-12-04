a3c2393492d71f5c2714414ddc37f7f3c95c67e8
Excel619 <excel8392@gmail.com>
2023-12-04 12:59:45 -0500

    Updated build.gradle.kts to improved superproject version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,3 @@
-val artifactName = "tablist"
-val rrGroup: String by rootProject.extra
-val rrVersion: String by rootProject.extra
-
-plugins {
-    `java-library`
-    `maven-publish`
-}
-
-group = rrGroup
-version = rrVersion
-
 dependencies {
     compileOnly(commonLibs.spigot)
     compileOnly(commonLibs.paper)
@@ -19,19 +7,4 @@ dependencies {
     compileOnly(project(":Projects:Core"))
     compileOnly(project(":Projects:Database"))
     compileOnly(project(":Projects:Guilds"))
-}
-
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = rrGroup
-            artifactId = artifactName
-            version = rrVersion
-            from(components["java"])
-        }
-    }
 }
